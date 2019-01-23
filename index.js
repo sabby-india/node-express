819dee76a23fcdf6acab1052366f8ca8766d2737
Sabby <sabby_india@hotmail.com>
2019-01-23 16:23:46 -0600

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express(); //create an express application
-
+app.use(morgan('dev')); //using morgan handler for development
+app.use(express.static(__dirname + '/public'));
 app.use((req,res,next) =>{
     console.log(req.header);
     res.statusCode = 200;
@@ -13,6 +15,7 @@ app.use((req,res,next) =>{
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
 
 const server = http.createServer(app);
 
